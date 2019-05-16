7dcb3ee4c0dc21518640e3570f36bb47671aba0d
MATHEUS PANTOJA <matheus.pantoja97@gmail.com>
2019-05-16 09:26:20 -0300

    .

---
+++ db.js
@@ -0,0 +1,27 @@
+module.exports = () => 
+
+	data = {
+		segments: [
+			{
+				id: 1,
+				title: 'supermercados',
+				image: __dirname+'\\shopping-cart.png'
+			},
+			{
+				id: 2,
+				title: 'farmarcia',
+				image: __dirname+'\\pills.svg',
+			},
+			{
+				id: 3,
+				title: 'ferramentas',
+				image: __dirname+'\\tools.svg.svg',
+			},
+			{
+				id: 4,
+				title: 'pet',
+				image: __dirname+'\\pawprint.svg',
+			},
+		],
+   }
+
